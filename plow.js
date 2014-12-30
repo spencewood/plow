b361247cff3f8f724bf9d6aa4957d7c70c152466
Tyler Spencewood <tyler.spencewood@ave81.com>
2014-12-29 20:57:53 -0600

    Update for recursive directories.

--- plow.js
+++ plow.js
@@ -5,7 +5,8 @@ var exec = require('child_process').exec;
 var program = require('commander');
 var yaml = require('js-yaml');
 var Promise = require('bluebird');
-var async = require('async');
+var ProgressBar = require('progress');
+var recursive = require('recursive-readdir');
 var _ = require('lodash');
 
 var getConfig = function(){
@@ -25,17 +26,50 @@ var exit = function(){
 };
 
 var fileInfo = function(path, cb){
-  return fs.stat(path, function(err, stats){
-    cb(err, {
-      path: path,
-      isFile: stats.isFile()
+  return new Promise(function(res, rej){
+    fs.stat(path, function(err, stats){
+      if(err){
+        return rej(err);
+      }
+      return res({
+        path: path,
+        isFile: stats.isFile(),
+        isDirectory: stats.isDirectory()
+      })
     });
   });
 };
 
 var parsePaths = function(paths){
-  var map = Promise.promisify(async.map);
-  return map(paths, fileInfo);
+  return Promise.map(paths, fileInfo);
+};
+
+var parsePathsRecursive = function(paths){
+  var pluck = function(prop){
+    return function(arr){
+      return _.pluck(arr, prop);
+    };
+  };
+
+  return parsePaths(paths).then(function(files){
+      return files.filter(function(path){
+        return path.isDirectory;
+      });
+    })
+    .then(pluck('path'))
+    .map(function(dir){
+      return new Promise(function(res, rej){
+        recursive(dir, function(err, files){
+          if(err){
+            return rej(err);
+          }
+          return res(files);
+        });
+      });
+    })
+    .then(_.flatten)
+    .then(_.compact)
+    .map(fileInfo);
 };
 
 var getFiltersByRegex = function(path){
@@ -54,10 +88,13 @@ var getApplicableFilter = function(file){
 };
 
 var getApplicableFilters = function(files){
-  return _.chain(files)
-          .map(getApplicableFilter)
-          .compact()
-          .value();
+  return _.reduce(files, function(memo, file){
+    var filter = getApplicableFilter(file);
+    if(filter != null){
+      memo.push(filter);
+    }
+    return memo;
+  }, []);
 };
 
 var constructCommand = function(filter){
@@ -71,9 +108,35 @@ var constructCommands = function(filters){
   return _.map(filters, constructCommand);
 };
 
+var execute = function(command){
+  return new Promise(function(res, rej){
+    exec(command, function(err, stdout){
+      if(err){
+        rej(err);
+      }
+      else{
+        res(stdout);
+      }
+    });
+  });
+};
+
 var runCommands = function(commands){
-  var eachSeries = Promise.promisify(async.eachSeries);
-  return eachSeries(commands, exec);
+  var bar = new ProgressBar(
+    '[:bar] :current/:total',
+    {
+      total: commands.length,
+      width: 20
+    }
+  );
+
+  var tick = function(){
+    bar.tick();
+  };
+
+  return Promise.map(commands, function(command){
+    return execute(command).then(tick);
+  });
 };
 
 var config = getConfig().plow;
@@ -82,9 +145,13 @@ program
   .version('0.0.1')
   .parse(process.argv);
 
-parsePaths(program.args).then(_.compose(
-  console.log,
-  runCommands,
-  constructCommands,
-  getApplicableFilters
-));
+parsePathsRecursive(program.args)
+  .then(getApplicableFilters)
+  .then(constructCommands)
+  .then(runCommands)
+  .catch(function(err){
+    console.error('error', err);
+  })
+  .then(function(){
+    exit();
+  });
